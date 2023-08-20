f0daa9fa14fbc45ef053f0b17b62945298c74636
Angelo Melonas <angelomelonas@gmail.com>
2023-08-20 09:45:22 +0200

    Add some rough scripts to simulate projectiles visually.

---
+++ scripts/projectiles/RunProjectile.php
@@ -0,0 +1,49 @@
+<?php
+
+use PhpRayTracer\RayTracer\Canvas\Canvas;
+use PhpRayTracer\RayTracer\Projectiles\Environment;
+use PhpRayTracer\RayTracer\Projectiles\Projectile;
+use PhpRayTracer\RayTracer\Projectiles\World;
+use PhpRayTracer\RayTracer\Tuple\ColorFactory;
+use PhpRayTracer\RayTracer\Tuple\TupleFactory;
+
+require 'vendor/autoload.php';
+
+$projectile = new Projectile(
+    TupleFactory::createPoint(0, 1, 0),
+    TupleFactory::createVector(1, 1.8, 0)->normalize()->multiply(11.25)
+);
+
+$environment = new Environment(
+    TupleFactory::createVector(0, -0.1, 0),
+    TupleFactory::createVector(-0.01, 0, 0)
+);
+
+$world = new World();
+
+$width = 900;
+$height = 550;
+$canvas = new Canvas($width, $height);
+
+$ticks = 0;
+while (true) {
+    $canvas->writePixel(
+        intval($projectile->position->x),
+        intval($height - $projectile->position->y),
+        ColorFactory::create(1, 1, 1)
+    );
+    $projectile = $world->tick($projectile, $environment);
+
+    echo sprintf("Projectile position: %s" . PHP_EOL, $projectile->position->__toString());
+
+    if ($projectile->position->y < 0) {
+        echo sprintf(PHP_EOL . "Projectile hit the ground at position %s after %s seconds." . PHP_EOL, $projectile->position->__toString(), $ticks++);
+        break;
+    }
+}
+
+$ppm = $canvas->canvasToPPM();
+file_put_contents('projectile.ppm', $ppm);
+
+
+
