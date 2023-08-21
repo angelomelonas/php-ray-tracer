1406efc7dfeea4bd040dbb31f3eacfcfee57a3e5
Angelo Melonas <angelomelonas@gmail.com>
2023-08-21 21:07:43 +0200

    Small improvements.

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,14 @@
 <?php
+declare(strict_types=1);
+
+use FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle;
+use Symfony\Bundle\DebugBundle\DebugBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MakerBundle\MakerBundle;
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    FrameworkBundle::class => ['all' => true],
+    FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    DebugBundle::class => ['dev' => true],
+    MakerBundle::class => ['dev' => true],
 ];
--- tests/Behat/DemoContext.php
+++ tests/Behat/DemoContext.php
@@ -1,10 +1,10 @@
 <?php
-
 declare(strict_types=1);
 
 namespace PhpRayTracer\Tests\Behat;
 
 use Behat\Behat\Context\Context;
+use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -17,32 +17,23 @@ use Symfony\Component\HttpKernel\KernelInterface;
  */
 final class DemoContext implements Context
 {
-    /** @var KernelInterface */
-    private KernelInterface $kernel;
-
-    /** @var Response|null */
     private ?Response $response;
 
-    public function __construct(KernelInterface $kernel)
+    public function __construct(private KernelInterface $kernel)
     {
-        $this->kernel = $kernel;
     }
 
-    /**
-     * @When a demo scenario sends a request to :path
-     */
+    /** @When a demo scenario sends a request to :path */
     public function aDemoScenarioSendsARequestTo(string $path): void
     {
         $this->response = $this->kernel->handle(Request::create($path, 'GET'));
     }
 
-    /**
-     * @Then the response should be received
-     */
+    /** @Then the response should be received */
     public function theResponseShouldBeReceived(): void
     {
         if ($this->response === null) {
-            throw new \RuntimeException('No response received');
+            throw new RuntimeException('No response received');
         }
     }
 }
