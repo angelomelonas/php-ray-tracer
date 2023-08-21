7090b3940c042f45b4e3e5c5aeccd3dd0552af86
Angelo Melonas <angelomelonas@gmail.com>
2023-08-21 20:57:06 +0200

    Added the required classes and functions for intersecting spheres with rays.

---
+++ features/spheres.feature
@@ -0,0 +1,129 @@
+Feature: Spheres
+
+  Scenario: A ray intersects a sphere at two points
+    Given r is a ray(point(0, 0, -5), vector(0, 0, 1))
+    And s is a sphere()
+    When xs is a intersect(s, r)
+    Then xs.count = 2
+    And xs[0] = 4.0
+    And xs[1] = 6.0
+
+  Scenario: A ray intersects a sphere at a tangent
+    Given r is a ray(point(0, 1, -5), vector(0, 0, 1))
+    And s is a sphere()
+    When xs is a intersect(s, r)
+    Then xs.count = 2
+    And xs[0] = 5.0
+    And xs[1] = 5.0
+
+  Scenario: A ray misses a sphere
+    Given r is a ray(point(0, 2, -5), vector(0, 0, 1))
+    And s is a sphere()
+    When xs is a intersect(s, r)
+    Then xs.count = 0
+
+  Scenario: A ray originates inside a sphere
+    Given r is a ray(point(0, 0, 0), vector(0, 0, 1))
+    And s is a sphere()
+    When xs is a intersect(s, r)
+    Then xs.count = 2
+    And xs[0] = -1.0
+    And xs[1] = 1.0
+
+  Scenario: A sphere is behind a ray
+    Given r is a ray(point(0, 0, 5), vector(0, 0, 1))
+    And s is a sphere()
+    When xs is a intersect(s, r)
+    Then xs.count = 2
+    And xs[0] = -6.0
+    And xs[1] = -4.0
+
+  Scenario: Intersect sets the object on the intersection
+    Given r is a ray(point(0, 0, -5), vector(0, 0, 1))
+    And s is a sphere()
+    When xs is a intersect(s, r)
+    Then xs.count = 2
+    And xs[0].object = s
+    And xs[1].object = s
+
+  Scenario: A sphere's default transformation
+    Given s is a sphere()
+    Then s.transform = identity_matrix
+
+  Scenario: Changing a sphere's transformation
+    Given s is a sphere()
+    And t is a translation(2, 3, 4)
+    When set_transform(s, t)
+    Then s.transform = t
+
+  Scenario: Intersecting a scaled sphere with a ray
+    Given r is a ray(point(0, 0, -5), vector(0, 0, 1))
+    And s is a sphere()
+    When set_transform(s, scaling(2, 2, 2))
+    And xs is a intersect(s, r)
+    Then xs.count = 2
+    And xs[0].t = 3
+    And xs[1].t = 7
+
+  Scenario: Intersecting a translated sphere with a ray
+    Given r is a ray(point(0, 0, -5), vector(0, 0, 1))
+    And s is a sphere()
+    When set_transform(s, translation(5, 0, 0))
+    And xs is a intersect(s, r)
+    Then xs.count = 0
+
+#  Scenario: The normal on a sphere at a point on the x axis
+#    Given s is a sphere()
+#    When n is a normal_at(s, point(1, 0, 0))
+#    Then n = vector(1, 0, 0)
+#
+#  Scenario: The normal on a sphere at a point on the y axis
+#    Given s is a sphere()
+#    When n is a normal_at(s, point(0, 1, 0))
+#    Then n = vector(0, 1, 0)
+#
+#  Scenario: The normal on a sphere at a point on the z axis
+#    Given s is a sphere()
+#    When n is a normal_at(s, point(0, 0, 1))
+#    Then n = vector(0, 0, 1)
+#
+#  Scenario: The normal on a sphere at a nonaxial point
+#    Given s is a sphere()
+#    When n is a normal_at(s, point(√3/3, √3/3, √3/3))
+#    Then n = vector(√3/3, √3/3, √3/3)
+#
+#  Scenario: The normal is a normalized vector
+#    Given s is a sphere()
+#    When n is a normal_at(s, point(√3/3, √3/3, √3/3))
+#    Then n = normalize(n)
+#
+#  Scenario: Computing the normal on a translated sphere
+#    Given s is a sphere()
+#    And set_transform(s, translation(0, 1, 0))
+#    When n is a normal_at(s, point(0, 1.70711, -0.70711))
+#    Then n = vector(0, 0.70711, -0.70711)
+#
+#  Scenario: Computing the normal on a transformed sphere
+#    Given s is a sphere()
+#    And m is a scaling(1, 0.5, 1) * rotation_z(π/5)
+#    And set_transform(s, m)
+#    When n is a normal_at(s, point(0, √2/2, -√2/2))
+#    Then n = vector(0, 0.97014, -0.24254)
+#
+#  Scenario: A sphere has a default material
+#    Given s is a sphere()
+#    When m is a s.material
+#    Then m = material()
+#
+#  Scenario: A sphere may be assigned a material
+#    Given s is a sphere()
+#    And m is a material()
+#    And m.ambient is a 1
+#    When s.material is a m
+#    Then s.material = m
+#
+#  Scenario: A helper for producing a sphere with a glassy material
+#    Given s is a glass_sphere()
+#    Then s.transform = identity_matrix
+#    And s.material.transparency = 1.0
+#    And s.material.refractive_index = 1.5
---
+++ tests/Behat/RayContext.php
@@ -0,0 +1,114 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpRayTracer\Tests\Behat;
+
+use _PHPStan_7c8075089\Symfony\Component\Console\Exception\LogicException;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use PhpRayTracer\RayTracer\Matrix\Matrix;
+use PhpRayTracer\RayTracer\Matrix\MatrixFactory;
+use PhpRayTracer\RayTracer\Ray\Ray;
+use PhpRayTracer\RayTracer\Ray\RayFactory;
+use PhpRayTracer\RayTracer\Tuple\Tuple;
+use PhpRayTracer\RayTracer\Tuple\TupleFactory;
+use PHPUnit\Framework\Assert;
+
+final class RayContext implements Context
+{
+    public Ray $rayA;
+    public Ray $rayB;
+    private Matrix $transformationMatrix;
+    private TupleContext $tupleContext;
+
+    /** @BeforeScenario */
+    public function gatherContexts(BeforeScenarioScope $scope): void
+    {
+        $environment = $scope->getEnvironment();
+
+        /* @phpstan-ignore-next-line */
+        $this->tupleContext = $environment->getContext(TupleContext::class);
+    }
+
+    /** @When /^(r) is a ray\((origin), (direction)\)$/ */
+    public function rIsARayOriginDirection(): void
+    {
+        $this->createRay($this->tupleContext->tupleA, $this->tupleContext->tupleB);
+    }
+
+    /** @Then /^(r)\.origin = (origin)$/ */
+    public function rOriginOrigin(): void
+    {
+        Assert::assertSame($this->tupleContext->tupleA, $this->rayA->origin);
+    }
+
+    /** @Given /^(r)\.direction = (direction)$/ */
+    public function rDirectionDirection(): void
+    {
+        Assert::assertTrue($this->tupleContext->tupleB->isEqualTo($this->rayA->direction));
+    }
+
+    /** @Given /^(r) is a ray\(point\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+)\), vector\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+)\)\)$/ */
+    public function rIsARayWithAOriginAndDirection(string $expression, float $x1, float $y1, float $z1, float $x2, float $y2, float $z2): void
+    {
+        $point = TupleFactory::createPoint($x1, $y1, $z1);
+        $vector = TupleFactory::createVector($x2, $y2, $z2);
+
+        $this->createRay($point, $vector);
+    }
+
+    /** @Then /^position\((r), ([-+]?\d*\.?\d+)\) = point\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+)\)$/ */
+    public function positionIsEqualToPoint(string $r, float $t, float $x, float $y, float $z): void
+    {
+        $expected = $this->rayA->position($t);
+        $actual = TupleFactory::createPoint($x, $y, $z);
+        Assert::assertTrue($expected->isEqualTo($actual));
+    }
+
+    /** @When /^(r2) is a transform\((r), (m)\)$/ */
+    public function risATransformRM(): void
+    {
+        $this->rayB = $this->rayA->transform($this->transformationMatrix);
+    }
+
+    /** @Then /^(r2)\.origin = point\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+)\)$/ */
+    public function rOriginIsPoint(string $expression, float $x, float $y, float $z): void
+    {
+        Assert::assertTrue(TupleFactory::createPoint($x, $y, $z)->isEqualTo($this->rayB->origin));
+    }
+
+    /** @Given /^(r2)\.direction = vector\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+)\)$/ */
+    public function rDirectionIsVector(string $expression, float $x, float $y, float $z): void
+    {
+        Assert::assertTrue(TupleFactory::createVector($x, $y, $z)->isEqualTo($this->rayB->direction));
+    }
+
+    /** @Given /^(m) is a translation\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+)\)$/ */
+    public function transformIsATranslation(string $expression, float $x, float $y, float $z): void
+    {
+        $this->transformationMatrix = MatrixFactory::createTranslation($x, $y, $z);
+    }
+
+    /** @Given /^(m) is a scaling\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+)\)$/ */
+    public function transformIsAScaling(string $expression, float $x, float $y, float $z): void
+    {
+        $this->transformationMatrix = MatrixFactory::createScaling($x, $y, $z);
+    }
+
+    private function createRay(Tuple $origin, Tuple $direction): Ray
+    {
+        if (! isset($this->rayA)) {
+            $this->rayA = RayFactory::create($origin, $direction);
+
+            return $this->rayA;
+        }
+
+        if (! isset($this->rayB)) {
+            $this->rayB = RayFactory::create($origin, $direction);
+
+            return $this->rayB;
+        }
+
+        throw new LogicException('No ray is set.');
+    }
+}
